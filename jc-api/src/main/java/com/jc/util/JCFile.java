bed7b5ee00d0552513e7669660017b76b7180119
Kristo Aun <kristo@medisoft.ee>
2015-01-27 13:22:57 +0200

    generic file handling class

---
+++ jc-api/src/main/java/com/jc/util/JCFile.java
@@ -0,0 +1,18 @@
+package com.jc.util;
+
+import javax.activation.MimetypesFileTypeMap;
+import java.io.File;
+
+public final class JCFile {
+    public static String getContentType(String file) {
+        return MimetypesFileTypeMap
+                .getDefaultFileTypeMap()
+                .getContentType(file);
+    }
+
+    public static String getContentType(File file) {
+        return MimetypesFileTypeMap
+                .getDefaultFileTypeMap()
+                .getContentType(file);
+    }
+}

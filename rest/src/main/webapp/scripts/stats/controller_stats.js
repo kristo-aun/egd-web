44ed01f4394e9ac4f544fd17e582014507537d82
Kristo Aun <kristoaun@live.com>
2015-01-28 23:22:14 +0200

    jmdict bar chart

--- rest/src/main/webapp/scripts/stats/controller_stats.js
+++ rest/src/main/webapp/scripts/stats/controller_stats.js
@@ -2,48 +2,34 @@
 
 egdApp.controller('StatsController', function ($scope, $log, StatsService) {
     $log.debug("StatsController");
-    StatsService.counters().then(function(data) {
+    StatsService.counters().then(function (data) {
         $scope.counters = data;
     });
-    StatsService.coreStats().then(function(data) {
+    StatsService.coreStats().then(function (data) {
         $scope.coreStats = data;
     });
-    StatsService.translatedEntrRatio().then(function(data) {
-        $scope.translatedEntrRatio = data;
+    StatsService.translatedEntrRatio().then(function (data) {
+        $log.debug("StatsController.translatedEntrRatio: data=", data);
+        $scope.translatedEntrRatio = [
+            $.map(data, function (item) {
+                return [item.countGloss, item.sumCount]
+            })
+        ];
     });
 
-    $scope.data = [[
-        ['Heavy Industry', 12],['Retail', 9], ['Light Industry', 14],
-        ['Out of home', 16],['Commuting', 7], ['Orientation', 9]
-    ]];
-
-    $scope.chartOptions = {
-        seriesDefaults: {
-            // Make this a pie chart.
-            renderer: jQuery.jqplot.PieRenderer,
-            rendererOptions: {
-                // Put data labels on the pie slices.
-                // By default, labels show the percentage of the slice.
-                showDataLabels: true
-            }
-        },
-        legend: { show:true, location: 'e' }
-    };
-
     $scope.translatedEntrRatioChartOpts = {
-        seriesDefaults: {
-            // Make this a pie chart.
-            renderer: jQuery.jqplot.PieRenderer,
-                rendererOptions: {
-                // Put data labels on the pie slices.
-                // By default, labels show the percentage of the slice.
-                showDataLabels: true
-            }
-        },
-        legend: { show:false, location: 'e' }
+
+        title: 'Bar Chart with Point Labels',
+        seriesDefaults: {renderer: $.jqplot.BarRenderer},
+        axes: {
+            xaxis: {
+                renderer:$.jqplot.CategoryAxisRenderer
+            },
+            yaxis: {padMax: 1}
+        }
     };
 
-    StatsService.countGlossToSumFreq().then(function(data) {
+    StatsService.countGlossToSumFreq().then(function (data) {
         $scope.countGlossToSumFreq = data;
     });
 });

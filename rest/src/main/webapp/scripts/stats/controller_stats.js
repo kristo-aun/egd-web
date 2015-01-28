f0c61679a06cf274ee00d0f46e0d92c99582b824
Kristo Aun <kristoaun@live.com>
2015-01-28 21:22:32 +0200

    pie chart

--- rest/src/main/webapp/scripts/stats/controller_stats.js
+++ rest/src/main/webapp/scripts/stats/controller_stats.js
@@ -11,6 +11,38 @@ egdApp.controller('StatsController', function ($scope, $log, StatsService) {
     StatsService.translatedEntrRatio().then(function(data) {
         $scope.translatedEntrRatio = data;
     });
+
+    $scope.data = [[
+        ['Heavy Industry', 12],['Retail', 9], ['Light Industry', 14],
+        ['Out of home', 16],['Commuting', 7], ['Orientation', 9]
+    ]];
+
+    $scope.chartOptions = {
+        seriesDefaults: {
+            // Make this a pie chart.
+            renderer: jQuery.jqplot.PieRenderer,
+            rendererOptions: {
+                // Put data labels on the pie slices.
+                // By default, labels show the percentage of the slice.
+                showDataLabels: true
+            }
+        },
+        legend: { show:true, location: 'e' }
+    };
+
+    $scope.translatedEntrRatioChartOpts = {
+        seriesDefaults: {
+            // Make this a pie chart.
+            renderer: jQuery.jqplot.PieRenderer,
+                rendererOptions: {
+                // Put data labels on the pie slices.
+                // By default, labels show the percentage of the slice.
+                showDataLabels: true
+            }
+        },
+        legend: { show:false, location: 'e' }
+    };
+
     StatsService.countGlossToSumFreq().then(function(data) {
         $scope.countGlossToSumFreq = data;
     });

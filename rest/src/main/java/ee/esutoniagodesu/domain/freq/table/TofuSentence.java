0fa925d1f6615b0546b3237df21db4ac10656a9b
Kristo Aun <kristo@medisoft.ee>
2015-02-03 14:08:19 +0200

    togu sentence domain class

---
+++ rest/src/main/java/ee/esutoniagodesu/domain/freq/table/TofuSentence.java
@@ -0,0 +1,77 @@
+package ee.esutoniagodesu.domain.freq.table;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Collection;
+
+@Table(name = "tofu_sentence", schema = "freq", catalog = "egd")
+@Entity
+public class TofuSentence implements Serializable {
+
+    private Integer id;
+    private String word;
+    private String sentence;
+
+    @SequenceGenerator(name = "seq", sequenceName = "freq.tofu_sentence_id_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq")
+    @Column(name = "id", nullable = false, insertable = true, updatable = true, length = 10, precision = 0)
+    @Id
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Column(name = "kanji", nullable = false, insertable = true, updatable = true, length = 2147483647, precision = 0)
+    @Basic
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    @Column(name = "sentence", nullable = false, insertable = true, updatable = true, length = 2147483647, precision = 0)
+    @Basic
+    public String getSentence() {
+        return sentence;
+    }
+
+    public void setSentence(String sentence) {
+        this.sentence = sentence;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        TofuSentence that = (TofuSentence) o;
+
+        if (id != null ? !id.equals(that.id) : that.id != null) return false;
+        if (sentence != null ? !sentence.equals(that.sentence) : that.sentence != null) return false;
+        if (word != null ? !word.equals(that.word) : that.word != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (word != null ? word.hashCode() : 0);
+        result = 31 * result + (sentence != null ? sentence.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "TofuSentence{" +
+            "id=" + id +
+            ", word='" + word + '\'' +
+            ", sentence='" + sentence + '\'' +
+            '}';
+    }
+}

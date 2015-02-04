06cdb4ce3feb8b70467253fda215f9487ffdde85
Kristo Aun <kristoaun@live.com>
2015-02-04 22:43:08 +0200

    tofu backend

---
+++ rest/src/main/java/ee/esutoniagodesu/web/rest/TofuResource.java
@@ -0,0 +1,47 @@
+package ee.esutoniagodesu.web.rest;
+
+import ee.esutoniagodesu.domain.ac.table.User;
+import ee.esutoniagodesu.domain.freq.table.TofuSentence;
+import ee.esutoniagodesu.security.AuthoritiesConstants;
+import ee.esutoniagodesu.service.TofuService;
+import ee.esutoniagodesu.service.UserService;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.security.RolesAllowed;
+import javax.inject.Inject;
+import java.util.List;
+
+@RestController
+@RequestMapping("/app")
+public class TofuResource {
+
+    @Inject
+    private TofuService service;
+
+    @Inject
+    private UserService userService;
+
+    private User getSessionUser() {
+        return userService.getUserWithAuthorities();
+    }
+
+    @RequestMapping(value = "/rest/tofus",
+        method = RequestMethod.POST,
+        produces = MediaType.APPLICATION_JSON_VALUE)
+    @RolesAllowed(AuthoritiesConstants.ADMIN)
+    public void save(@RequestBody TofuSentence tofu) {
+        service.save(tofu, getSessionUser());
+    }
+
+    @RequestMapping(value = "/rest/tofus",
+        method = RequestMethod.GET,
+        produces = MediaType.APPLICATION_JSON_VALUE)
+    @RolesAllowed(AuthoritiesConstants.ADMIN)
+    public List<TofuSentence> getAll() {
+        return service.getTofusByUser(getSessionUser());
+    }
+}

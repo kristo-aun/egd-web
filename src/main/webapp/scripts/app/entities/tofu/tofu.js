4b78e25fd269525d8da3c3e058b1d74422f2a00b
Kristo Aun <kristoaun@live.com>
2015-06-23 19:14:14 +0300

    tofu lausete filtrit ei tule

--- src/main/webapp/scripts/app/entities/tofu/tofu.js
+++ src/main/webapp/scripts/app/entities/tofu/tofu.js
@@ -22,25 +22,5 @@ egdApp
                         return $translate.refresh();
                     }]
                 }
-            })
-            .state('tofuDetail', {
-                parent: 'tofu',
-                url: '/tofu/:id',
-                data: {
-                    roles: ['ROLE_USER'],
-                    pageTitle: 'tofu.detail.title'
-                },
-                views: {
-                    'content@': {
-                        templateUrl: 'scripts/app/entities/tofu/tofu.detail.html',
-                        controller: 'TofuDetailController'
-                    }
-                },
-                resolve: {
-                    translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
-                        $translatePartialLoader.addPart('tofu');
-                        return $translate.refresh();
-                    }]
-                }
             });
     });

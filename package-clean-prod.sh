ae5254fb7904093d4bc21fb548313508b1180bc9
Kristo Aun <kristoaun@live.com>
2015-08-02 19:45:22 +0300

    logback hardcoded destination

--- package-clean-prod.sh
+++ package-clean-prod.sh
@@ -3,5 +3,5 @@
 #mvn install spring-boot:repackage -Dmaven.test.skip=true
 mvn clean package -Dmaven.test.skip=true -Pprod -e
 
-rm target/ROOT.war
-mv target/ROOT.war.original target/ROOT.war
+#rm target/ROOT.war
+#mv target/ROOT.war.original target/ROOT.war

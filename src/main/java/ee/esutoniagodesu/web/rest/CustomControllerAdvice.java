a84a3281bf904952a654f46ba1ba1b27dd16549d
Kristo Aun <kristoaun@live.com>
2015-08-09 11:44:20 +0300

    maybe this helpts against 'connection reset by peer'

--- src/main/java/ee/esutoniagodesu/web/rest/CustomControllerAdvice.java
+++ src/main/java/ee/esutoniagodesu/web/rest/CustomControllerAdvice.java
@@ -1,16 +1,16 @@
 package ee.esutoniagodesu.web.rest;
 
+import org.apache.catalina.connector.ClientAbortException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.support.GenericMessage;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
-import java.nio.file.AccessDeniedException;
 
 @ControllerAdvice(annotations = RestController.class)
 public class CustomControllerAdvice extends ResponseEntityExceptionHandler {
@@ -27,6 +27,12 @@ public class CustomControllerAdvice extends ResponseEntityExceptionHandler {
     public ResponseEntity<GenericMessage<String>> forbidden(Exception e) {
         log.error("forbidden {}", e.getMessage());
         return ResponseEntity.status(HttpStatus.FORBIDDEN).body(messageOf(e));
+    }
+
+    @ExceptionHandler({ClientAbortException.class})
+    public ResponseEntity<GenericMessage<String>> connection_reset_by_peer(Exception e) {
+        log.error("connection_reset_by_peer {}", e.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(messageOf(new RuntimeException(e.getMessage())));
     }
 
     @ExceptionHandler({IllegalStateException.class})
